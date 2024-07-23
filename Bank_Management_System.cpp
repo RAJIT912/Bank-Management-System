425d7097384e902ca7d218de58ec8e2ed2d981fd
RAJIT912 <126567485+RAJIT912@users.noreply.github.com>
2024-07-23 17:07:08 +0530

    Update Bank_Management_System.cpp

--- Bank_Management_System.cpp
+++ Bank_Management_System.cpp
@@ -163,7 +163,7 @@ int main()
 			break;
 		case '8':
 			system("CLS");
-			cout << "\n\n\t\t\tBrought To You By Ahmad And Abdul Rehman";
+			cout << "\n\n\t\t\tThank You!!!";
 			break;
 		default:cout << "\a";
 		}
